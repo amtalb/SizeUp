2cb452f4ce26e6631dcc0249c85d041c6ca5f5ee
Alexander Talbott <atalbott13@gmail.com>
2023-05-14 22:51:19 -0400

    file clean up

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sass --style=compressed scss/sizeup.scss sizeup.min.css
-cp sizeup.min.css docs/
+cp sizeup.min.css public/
 
 elm make src/Main.elm --optimize --output=main.js
-cp main.js docs/
-cp index.html docs/
+cp main.js public/
+cp index.html public/
